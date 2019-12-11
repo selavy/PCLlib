24de7c520e654d45bf4be5bf0f5ee74c67259579
Peter Lesslie <pclesslie@gmail.com>
2019-12-11 16:53:21 -0600

    Set up unit tests

---
+++ include/PCLlib/pcltable.h
@@ -0,0 +1,9 @@
+#ifndef PCLTABLE__H_
+#define PCLTABLE__H_
+
+struct PCLtable {
+    int* keys;
+    int* vals;
+};
+
+#endif
---
+++ tests/test_pcltable.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch.hpp>
+#include "PCLlib/pcltable.h"
+
+TEST_CASE("Should fail")
+{
+    REQUIRE(true == false);
+}
