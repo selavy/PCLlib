d3f4e7fd1d9ed46e8b0d5b8773fb6b3b1c0b63e0
Peter Lesslie <pclesslie@gmail.com>
2019-12-16 11:59:28 -0600

    Add c90 test to leak check

--- tests/check_vector_leaks.sh
+++ tests/check_vector_leaks.sh
@@ -2,20 +2,58 @@
 
 BIN=./build/
 OUTPUT=leak_check.output
+failures=0
 
+#
+# C++ Tests
+#
+echo "Running C++ tests..."
 valgrind ${BIN}/tests/unittest > /dev/null 2> $OUTPUT
 if [ $? -ne 0 ]
 then
-    echo "Failed to run unit tests!" >&2;
+    echo "Failed to run unit tests. Does the executable exist?" >&2;
     exit 1
 fi
 
-# TODO: check return code
 grep --quiet "no leaks are possible" $OUTPUT
-if [ $? -eq 0 ]; then
-    echo "Passed.";
-    rm $OUTPUT;
-else
+if [ $? -ne 0 ];
+then
     echo "Failed. Inspect $OUTPUT" >&2;
-    exit 1;
+    failures=failures + 1;
+else
+    echo "Passed."
+    rm $OUTPUT
+fi;
+
+#
+# C90 Tests
+#
+C90_TESTS=(test-pclvector-c90)
+for exe in ${C90_TESTS[@]};
+do
+    echo "Running ${exe}..."
+    output=leak_check.${exe}.output
+    valgrind ${BIN}/tests/${exe} > /dev/null 2> $output
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to run test. Does the executable exist?" >&2;
+        exit 1
+    fi
+    grep --quiet "no leaks are possible" $output
+    if [ $? -eq 0 ]
+    then
+        echo "Passed.";
+        rm $output
+    else
+        echo "Failed. Inspect $output" >&2;
+        failures=failures + 1;
+    fi;
+done;
+
+if [ $failures -ne 0 ]
+then
+    echo "Had ${failures} failures. :-(";
+    exit 1
+else
+    exit 0
 fi
